996768364206e1430a52f294105c77368e93f351
Osman Batuhan Kalkan <osmankalkan21@istanbularel.edu.tr>
2025-03-04 11:43:49 +0300

    Yerel değişiklikler eklendi

--- src/_001_Static_Anahtar_Sozcugu/Player.java
+++
@@ -1,9 +0,0 @@
-package _001_Static_Anahtar_Sozcugu;
-
-public class Player {
-    static int onlinePlayers;
-
-    Player() {
-        onlinePlayers++;
-    }
-}
--- src/_007_Kalitim_Inheritance_Ilkesi/Readme.md
+++
@@ -1,2 +0,0 @@
-![a.png](a.png)
-![a1.png](a1.png)
