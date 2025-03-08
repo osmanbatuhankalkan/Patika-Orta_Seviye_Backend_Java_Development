be00f97866cfbf4fd68d8c5bf126be21325e3401
Osman Batuhan Kalkan <osmankalkan21@istanbularel.edu.tr>
2025-03-08 23:52:33 +0300

    Add files via upload

---
+++ HackerRank-Java_Inheritance_II/src/Adder.java
@@ -0,0 +1,10 @@
+public class Adder extends Arithmetic{
+    Adder(){
+        super();//ust sınıfın cons
+        //cağrılır.
+    }
+}
+//Üst sınıfı extend edip, cons içerisine super methodula
+//üst sınıfın methodlarını fazladan kod yazmadan
+//bu sınıfımız çalışırtırılır,çalışmaz sanki kendi içinde
+//^ymiş gibi çağrır ve çalışır.
---
+++ HackerRank-Java_Inheritance_II/src/Arithmetic.java
@@ -0,0 +1,8 @@
+public class Arithmetic {
+    public Arithmetic() {
+    }
+    public int add(int num1,int num2 ){
+        return num1+num2;
+    }
+}
+//Sınıfımızı yarattık, methodumuzu tanımladık.
